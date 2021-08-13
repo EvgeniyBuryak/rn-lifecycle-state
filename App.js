6eb8fd5c868a2c32e541784faf914238730c140e
Jhonah <buryakone@gmail.com>
2021-08-13 16:31:54 +0700

    реализовал загрузку и вывод списка

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const NewVacancy = (props) => {
+    return (
+        <View>
+            <Text>{props.title}</Text>
+        </View>
+    );
+};
+
+const App = () => {
+    const [text, setText] = useState(' ');
+    const [vacancy, setVacancy] = useState([]);
+
+    return (
+        <View style={styles.container}>
+            <Text>Введите новую вакансию:</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="enter the new vacancy"
+                onChangeText={setText}
+                defaultValue={text}
+
+                onSubmitEditing={() => {
+                    // Такая вакансия уже существует?
+                    const isExist = vacancy.find(e => e == text);
+
+                    // Если нет, записываем
+                    if (!isExist)
+                        setVacancy([...vacancy, text]);
+
+                    console.log(vacancy);
+                }}
+            />
+            <Text>Список вакансий:</Text>
+            <FlatList
+                keyExtractor={(item) => item}
+                data={vacancy}
+                renderItem={({ item }) => {
+                    // item === string
+                    return <View>
+                        <Text>Вакансия: {item}</Text>
+                    </View>
+                }}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 50,
+    },
+    input: {
+        height: 40,
+        width: 200,
+        borderColor: 'gray',
+        borderWidth: 1,
+    }
 });
+
+export default App;
