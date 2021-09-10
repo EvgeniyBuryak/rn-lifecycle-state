4de4356087cfdce1547c0f6603bb0a8922cc579e
Jhonah <buryakone@gmail.com>
2021-09-10 16:09:29 +0700

    обновил структуру проекта

--- App.js
+++ App.js
@@ -1,62 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, ActivityIndicator, Button } from 'react-native';
-import useResults from './src/hooks/useResults';
-//import ViewToast from './src/components/viewToast'; 
-import Toast, { DURATION } from 'react-native-easy-toast';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/homeScreen/home-screen.view';
 
-const App = function () {    
-    const [vacancyApi, results, onLoader, errorMessage] = useResults();
-
-    App._toast = '';
-
-    useEffect(() => {
-        if (!onLoader) {
-            _toast.show(errorMessage, 2000);
-        }
-    });//, [errorMessage]);
-
-    //<Button title={'Press me'} onPress={vacancyApi} />
-
-    return (
-        <View style={styles.container}>
-            
-            <Toast ref={(toast) => _toast = toast}
-                position = 'top'
-            />    
-            <ActivityIndicator
-                size="large"
-                color="#0000ff"
-                animating={onLoader}
-                hidesWhenStopped={true}
-            />
-            <Text style={{ fontSize: 22 }}>Вакансии:</Text>
-            <FlatList
-                keyExtractor={(item) => item.id.toString()}
-                data={results}
-                renderItem={({ item }) => {
-                    return <View>
-                        <Text style={styles.titleSize}>
-                            {item.header}
-                        </Text>
-                    </View>
-                }}
-            />            
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 50,
+const navigator = createStackNavigator(
+    {
+        Home: HomeScreen
     },
-    titleSize: {
-        fontSize: 20,
-        margin: 5,
+    {
+        initialRouteName: 'Home',
+        defaultNavigationOptions: {
+            title: 'List Vacancies'
+        }
     }
-});
+);
 
-export default App;
+export default createAppContainer(navigator);
